8f68603463f98fcf174cb3faee66d21f7e5912c5
Milan Winter <milanwinter8@gmail.com>
2020-07-18 04:07:46 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = {drake:"pound cake"}
+function updatePlaylist(playlist,artistName,songTitle) {
+  playlist[artistName]=songTitle
+  return playlist;
+}
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
